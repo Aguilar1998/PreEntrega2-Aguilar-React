53d1181197576d5b17788d8cc32ef7877995434b
Martin Aguilar <aguilarmartin977@gmail.com>
2023-02-02 14:43:46 -0300

    Corrigiendo bug y conflicto de pull

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
     return (
         <BrowserRouter className='container mx-auto'>
             
-            <NavBar />
+            <NavBar/>
             <ExampleSection />
             
             <Routes>
